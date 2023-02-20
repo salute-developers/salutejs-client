41de09eaab71df85c8a464175a64e348021ff7bb
Evgeniy Semin <evgeniy.o.semin@gmail.com>
2023-02-20 13:49:41 +0300

    fix: Добавлены тесты автослушания

--- cypress/support/helpers/clientMethods.helpers.ts
+++ cypress/support/helpers/clientMethods.helpers.ts
@@ -2,34 +2,53 @@ import { Server } from 'mock-socket';
 
 import { appendHeader } from '../../../src/assistantSdk/client/protocol';
 import { Message, IText } from '../../../src/proto';
-import { MessageNames } from '../../../src/typings';
+import { SystemMessageDataType, MessageNames, Character } from '../../../src/typings';
 
-export const createAnswerBuffer = ({
-    messageId = 1,
-    last = 1,
-    systemMessageData,
-    messageName,
-    text,
-}: {
+type CreateAnswerBuffer1Params = {
     messageId?: number | Long;
     last?: number;
-    systemMessageData?: string;
+    systemMessage?: Partial<Omit<SystemMessageDataType, 'character'> & { character: Partial<Character> }>;
     messageName?: string;
     text?: IText;
-}) => {
+    appendVoice?: boolean;
+};
+
+export const createMessage = ({
+    messageId = 1,
+    last = 1,
+    systemMessage,
+    messageName = MessageNames.ANSWER_TO_USER,
+    text,
+    appendVoice = false,
+}: CreateAnswerBuffer1Params) => {
     const encodedAsNodeBuffer = appendHeader(
         Message.encode({
             messageId,
-            last: last ?? 1,
-            systemMessage: systemMessageData ? { data: systemMessageData } : { data: messageId.toString() },
-            messageName: messageName || MessageNames.ANSWER_TO_USER,
+            messageName,
+            systemMessage: {
+                data: systemMessage ? JSON.stringify(systemMessage) : messageId.toString(),
+            },
+            voice: !appendVoice ? undefined : {
+                data: Uint8Array.from({ length: 100 }, () => Math.floor(Math.random() * 5)),
+            },
             text,
+            last,
         }).finish(),
     );
     const newBuffer = new ArrayBuffer(encodedAsNodeBuffer.byteLength);
     const newBufferView = new Uint8Array(newBuffer);
+
     newBufferView.set(encodedAsNodeBuffer, 0);
+
     return newBuffer;
+};
+
+export const createVoiceMessage = (params: Omit<CreateAnswerBuffer1Params, 'appendVocie'> = {}) => {
+    return createMessage({
+        messageName: MessageNames.STT,
+        ...params,
+        appendVoice: true,
+    });
 };
 
 export const createServerPong = (server: Server) => {
@@ -40,7 +59,7 @@ export const createServerPong = (server: Server) => {
             if (!message.initialSettings) {
                 const delay = Number(message.text.data);
                 setTimeout(() => {
-                    socket.send(createAnswerBuffer({ messageId: message.messageId, last: message.last }));
+                    socket.send(createMessage({ messageId: message.messageId, last: message.last }));
                 }, delay);
             }
         });
