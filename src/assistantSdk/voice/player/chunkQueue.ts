97a1a64ba7c5537c0ab7f49c6e78a6d9b95b1e81
Alexander Salmin <sasha-tlt@yandex.ru>
2024-03-29 14:07:35 +0300

    fix: лагание озвучки на android

--- src/assistantSdk/voice/player/chunkQueue.ts
+++ src/assistantSdk/voice/player/chunkQueue.ts
@@ -1,19 +1,9 @@
 /** Создает структуру для хранения загружаемых и воспроизводимых частей трека */
 export const createChunkQueue = <T extends AudioBufferSourceNode>() => {
-    const buffer: Array<T> = []; // очередь на воспроизведение
     const chunks: Array<T> = []; // очередь воспроизведения
 
-    let duration = 0; // продолжительность очереди на воспроизведение, сек
-    let loaded = false; // флаг завершения загрузки
-
-    /** Добавить чанк в очередь на воспроизведение */
-    const push = (chunk: T) => {
-        buffer.push(chunk);
-        duration += chunk.buffer?.duration || 0;
-    };
-
     /** Добавить чанк в очередь воспроизведения */
-    const toPlay = (chunk: T) => {
+    const push = (chunk: T) => {
         chunks.push(chunk);
     };
 
@@ -22,41 +12,18 @@ export const createChunkQueue = <T extends AudioBufferSourceNode>() => {
         chunks.splice(chunks.indexOf(chunk), 1);
     };
 
-    /** Получить очередь на воспроизведение */
-    const popAll = (): Array<T> => {
-        duration = 0;
-        return buffer.splice(0, buffer.length);
-    };
-
-    /** Проставляем признак окончания загрузки трека */
-    const allLoaded = () => {
-        loaded = true;
-    };
-
     return {
-        get bufferLen() {
-            return buffer.length;
-        },
         get chunks() {
             return chunks;
         },
-        allLoaded,
-        toPlay,
         remove,
         push,
-        popAll,
         get length() {
             return chunks.length;
         },
-        get duration() {
-            return duration;
-        },
         get ended() {
-            // считаем трек законченным, когда все загружено и воспроизведено
-            return loaded && chunks.length === 0 && buffer.length === 0;
-        },
-        get loaded() {
-            return loaded;
+            // считаем трек законченным, когда все воспроизведено
+            return chunks.length === 0;
         },
     };
 };
