6c3de011b6b7b211832db8e8080ec52c7f9a6eaa
Alexander Salmin <sasha-tlt@yandex.ru>
2024-09-09 10:19:19 +0300

    feat: web-worker v2

--- src/assistantSdk/assistant.ts
+++ src/assistantSdk/assistant.ts
@@ -281,17 +281,7 @@ export const createAssistant = ({
         getVoiceMeta: () => (getVoiceMeta ? convertFieldValuesToString(getVoiceMeta()) : {}),
     });
     const voice = createVoice(
-        (() => ({
-            init: () => Promise.resolve(),
-            createVoiceStream: (func) =>
-                func({
-                    sendVoice: (params) => {
-                        window.postMessage(JSON.stringify({ voice: params }));
-                    },
-                    messageId: '123',
-                }),
-            on: () => () => {},
-        }))(),
+        client,
         settings,
         (event) => {
             if (typeof event.tts !== 'undefined') {
